a08a975e3b867a640d224a6c1ebd2678b629e377
Shepherd Smith <appliedchristian@gmail.com>
2024-02-13 21:13:41 -0500

    nicer formatting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 import msvcrt
+import os
+
+
 
 v = """Keep me safe, my God, for in you I take refuge.
 I say to the LORD, “You are my Lord; apart from you I have no good thing.”
@@ -13,37 +16,46 @@ Therefore my heart is glad and my tongue rejoices; my body also will rest secure
 because you will not abandon me to the realm of the dead, nor will you let your faithful one see decay.
 You make known to me the path of life; you will fill me with joy in your presence, with eternal pleasures at your right hand."""
 
-
-
+str_remove_list = ['“','"']
 words = v.split()
-
 index = 0
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 def first_letter_matches(a:str, b:str):
+    for char in str_remove_list:
+        a = a.replace(char,'')
+        b = b.replace(char,'')
     return (a[0].lower() == b[0].lower())
 
 def std_print(msg:str):
         sys.stdout.write(msg)
         sys.stdout.flush()
 
+cls()
+print("Psalm 16")
 while(index < len(words)):
     k_bytes = msvcrt.getch()
     k_str = str(k_bytes, encoding='utf-8')
 
+    # Exit Program
     if(k_str == "0"):
         index = len(words)+1
     
     else:
         word = words[index]
+
         if("." in word):
             delimiter = "\n"
         else:
             delimiter = " "
 
+        # Correct guess displays as normal
         if(first_letter_matches(k_str,word)):
             std_print(word+delimiter)
-
+        # Incorrect displays as bold and uppercase
         else:
-            std_print(word+"*"+delimiter)
+            std_print('\033[1m' + word.upper() + delimiter + '\033[0m')
 
         index += 1
